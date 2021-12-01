34c3f882ce5afabd3b836ec028fe928b5e0eb99d
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-30 22:26:40 -0800

    made solution part 2 a one liner for funsies

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -11,16 +11,21 @@ fn solve_part1(input: &str) -> i32 {
 }
 
 fn solve_part2(input: &str) -> i32 {
-    let nums = input
+    input
         .lines()
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<_>>();
-    (0..(nums.len() - 2))
-        .map(|i| nums[i..][..3].iter().sum())
-        .fold((0, i32::MAX), |(c, p), n| {
-            (if n > p { c + 1 } else { c }, n)
+        .map(|s| s.parse().ok())
+        .collect::<Option<Vec<_>>>()
+        .and_then(|nums| {
+            Some(
+                (0..(nums.len() - 2))
+                    .map(|i| nums[i..][..3].iter().sum())
+                    .fold((0, i32::MAX), |(c, p), n| {
+                        (if n > p { c + 1 } else { c }, n)
+                    })
+                    .0,
+            )
         })
-        .0
+        .unwrap()
 }
 
 fn main() {
