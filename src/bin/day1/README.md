3bfef593300f49d6c69c971b16bf41dd24170be0
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-30 21:32:14 -0800

    solve part 2

--- src/bin/day1/README.md
+++ src/bin/day1/README.md
@@ -1,1 +1,3 @@
-# Day 1
+# Day 1: Sonar Sweep
+
+Link to the puzzle: https://adventofcode.com/2021/day/1
--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,4 +1,4 @@
-fn solve(input: &'static str) -> usize {
+fn solve_part1(input: &'static str) -> usize {
     input
         .lines()
         .map(|s| s.parse::<usize>().unwrap())
@@ -8,13 +8,28 @@ fn solve(input: &'static str) -> usize {
         .0
 }
 
+fn solve_part2(input: &'static str) -> usize {
+    let nums = input
+        .lines()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    (0..(nums.len() - 2))
+        .map(|i| nums[i..][..3].iter().sum::<usize>())
+        .fold((0, None), |(c, p), n| {
+            (if n > p.unwrap_or(n) { c + 1 } else { c }, Some(n))
+        })
+        .0
+}
+
 fn main() {
     let input = include_str!("input.txt");
-    println!("SOLUTION: {}", solve(input));
+    println!("PART 1: {}", solve_part1(input));
+    println!("PART 2: {}", solve_part2(input));
 }
 
 #[test]
 fn test() {
     let input = include_str!("test.txt");
-    assert_eq!(solve(input), 7);
+    assert_eq!(solve_part1(input), 7);
+    assert_eq!(solve_part2(input), 5);
 }
