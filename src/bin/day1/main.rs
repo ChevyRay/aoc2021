2051ab5f170d0c126a72b5ad4b83c9184ab6c16c
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-30 21:11:27 -0800

    day 1 solution

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,3 +1,20 @@
+fn solve(input: &'static str) -> usize {
+    input
+        .lines()
+        .map(|s| s.parse::<usize>().unwrap())
+        .fold((0, None), |(c, p), n| {
+            (if n > p.unwrap_or(n) { c + 1 } else { c }, Some(n))
+        })
+        .0
+}
+
 fn main() {
-    println!("Hello!");
+    let input = include_str!("input.txt");
+    println!("SOLUTION: {}", solve(input));
+}
+
+#[test]
+fn test() {
+    let input = include_str!("test.txt");
+    assert_eq!(solve(input), 7);
 }
