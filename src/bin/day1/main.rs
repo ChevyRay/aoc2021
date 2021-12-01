ecea2db3e5d861a95a0520ad680ff4bb8f728746
Chevy Ray Johnston <happytrash@gmail.com>
2021-11-30 23:20:41 -0800

    keep collect() and no_collect() versions

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -13,6 +13,24 @@ fn solve_part1(input: &str) -> i32 {
 fn solve_part2(input: &str) -> i32 {
     input
         .lines()
+        .map(|s| s.parse().ok())
+        .collect::<Option<Vec<_>>>()
+        .and_then(|nums| {
+            Some(
+                nums.windows(3)
+                    .map(|n| n.iter().sum())
+                    .fold((0, i32::MAX), |(c, p), n| {
+                        (if n > p { c + 1 } else { c }, n)
+                    })
+                    .0,
+            )
+        })
+        .unwrap()
+}
+
+fn solve_part2_no_collect(input: &str) -> i32 {
+    input
+        .lines()
         .map(|s| s.parse::<i32>().unwrap())
         .fold((0, i32::MAX, None, None), |(n, p, a, b), c| {
             a.and_then(|a| {
@@ -41,8 +59,17 @@ fn main() {
     let solution2 = solve_part2(input);
     let time2 = Instant::now() - t;
 
+    let t = Instant::now();
+    let solution2_no_collect = solve_part2_no_collect(input);
+    let time2_no_collect = Instant::now() - t;
+
     println!("PART 1: {} ({} μs)", solution1, time1.as_micros());
     println!("PART 2: {} ({} μs)", solution2, time2.as_micros());
+    println!(
+        "PART 2: {} ({} μs) (no Vec allocation)",
+        solution2_no_collect,
+        time2_no_collect.as_micros()
+    );
 }
 
 #[test]
